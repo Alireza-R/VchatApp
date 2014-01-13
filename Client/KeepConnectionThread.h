adb7a75bc63c9da1e5e1e66dac1dbfb921d3d94c
Alireza Ranjbar <alireza.ranj@gmail.com>
2014-01-13 03:05:55 +0200

    Create KeepConnectionThread.h

---
+++ Client/KeepConnectionThread.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+#include <pthread.h>
+
+void* KeepConnectionThread(void* sock);
